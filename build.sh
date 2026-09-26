57e72d6a1e651eafd407f3c6762685ff795061dd
Bhavesh Sondagar <sondagarb590@gmail.com>
2026-09-26 17:30:30 +0530

    Change currency from dollar to rupee

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Auto-create a superuser from environment variables, if provided and not already existing
-if [[ -n "$DJANGO_SUPERUSER_USERNAME" && -n "$DJANGO_SUPERUSER_EMAIL" && -n "$DJANGO_SUPERUSER_PASSWORD" ]]; then
+if [[ -n "₹DJANGO_SUPERUSER_USERNAME" && -n "$DJANGO_SUPERUSER_EMAIL" && -n "$DJANGO_SUPERUSER_PASSWORD" ]]; then
   python manage.py shell -c "
 from django.contrib.auth import get_user_model
 User = get_user_model()
